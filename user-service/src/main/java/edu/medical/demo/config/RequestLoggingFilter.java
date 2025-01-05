edb26993b0f517f59f820911e83ed4f34d7864e2
DameerGamlet <gubaidulin.dam@gmail.com>
2025-01-05 14:48:20 +0400

    feat: добавление zipkin

--- user-service/src/main/java/edu/medical/demo/config/RequestLoggingFilter.java
+++ user-service/src/main/java/edu/medical/demo/config/RequestLoggingFilter.java
@@ -13,7 +13,8 @@ import java.io.IOException;
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         System.out.println("Request: " + request.getMethod() + " " + request.getRequestURI());
         filterChain.doFilter(request, response);
     }
